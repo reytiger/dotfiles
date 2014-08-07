1bd10327157036ee0323a3b15d53d71fc0dba4db
Rey Tresvolk <reytiger@earthlink.net>
2014-08-07 13:19:30 -0600

    Updated configs for new install. Added systemctl bash aliases.

--- .bashrc
+++ .bashrc
@@ -11,3 +11,8 @@ PS1='[\u@\h \W]\$ '
 #Swap the esc and tab keys
 echo keycode 1 = Caps_Lock | sudo loadkeys > logfile
 echo keycode 58 = Escape | sudo loadkeys > logfile
+
+#aliases to allow easy power control
+alias reboot="sudo systemctl reboot"
+alias poweroff="sudo systemctl poweroff"
+alias halt="sudo systemctl halt"
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,20 +1,8 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.EZConfig
-import XMonad.Util.Run
 
-main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar /home/rey/.xmobarrc"
-    xmonad $ defaultConfig
-	{
-        terminal = "terminology"
-        , modMask=mod4Mask
-        , borderWidth = 3
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , manageHook = manageHook defaultConfig <+> manageDocks
-	}
-    `additionalKeys`
-    [
-        ((mod4Mask, xK_w), spawn $ "/usr/bin/sudo -S /usr/bin/connman_dmenu")
-    ]
+main = xmonad defaultConfig
+    {
+        terminal        = "terminology"
+        , modMask       = mod4Mask
+        , borderWidth   = 3
+    }
