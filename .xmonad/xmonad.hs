e079a11d1c8d325ff1a8eff28196a782ffe44c58
Kevin Carbaugh <kcarbaug@mines.edu>
2016-05-20 18:35:15 -0600

    CSGO and attempting to customize xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,15 +2,26 @@ import XMonad
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.ManageHelpers
+--import XMonad.Util.Run(spawnPipe)
+
+
+myManageHook = composeAll
+        [ className =? "hl2_linux" --> doFullFloat
+        , className =? "csgo_linux" --> doFullFloat
+        , manageDocks
+        ]
+
+myLayoutHook = layoutHook defaultConfig ||| noBorders Full
 
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar /home/rey/.xmonad/xmobarrc"
-  xmonad $ defaultConfig{ layoutHook = smartBorders $ layoutHook defaultConfig
-    , manageHook = manageHook defaultConfig <+> manageDocks }
+  --xmproc <- spawnPipe "/usr/bin/xmobar /home/rey/.xmonad/xmobarrc"
+  xmonad $ defaultConfig{ layoutHook = myLayoutHook }
     {
       terminal          = "terminology"
-    , modMask           = mod4Mask
+    , modMask           = mod1Mask
     , borderWidth       = 3
     , focusedBorderColor = "#248F24"
+    , clickJustFocuses  = True
+    , manageHook = myManageHook <+> manageHook defaultConfig
     }
